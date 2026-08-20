bd2f814ffbcc7a74b0cc4109f90367d9ffd90fb6
zervaaa <izumisagiri842@gmail.com>
2026-08-20 17:01:56 +0700

    feat: initialize visual-debut theme with initial templates, regions, and configuration

--- config/themes.php
+++ config/themes.php
@@ -24,6 +24,24 @@ return [
                 'package_assets_directory' => 'src/Resources/assets',
             ],
         ],
+
+        'visual-debut' => [
+            'code' => 'visual-debut',
+            'name' => 'Visual Debut',
+            'version' => '2.0.0-alpha',
+            'author' => 'Bagisto Plus',
+            'assets_path' => 'public/themes/shop/visual-debut',
+            'views_path' => 'resources/themes/visual-debut/views',
+            'preview_image' => 'themes/shop/visual-debut/images/theme-preview.png',
+            'documentation_url' => 'https://visual.bagistoplus.com',
+            'visual_theme' => true,
+
+            'vite' => [
+                'hot_file' => 'themes/shop/visual-debut/visual-debut-vite.hot',
+                'build_directory' => 'themes/shop/visual-debut',
+                'package_assets_directory' => 'resources/assets',
+            ],
+        ],
     ],
 
     /*
