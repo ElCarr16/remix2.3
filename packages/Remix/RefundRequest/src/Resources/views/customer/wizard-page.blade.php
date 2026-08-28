aa0d2b5bf718187a644a42af39b5a98abacc243a
zervaaa <izumisagiri842@gmail.com>
2026-08-28 15:08:17 +0700

    fix(refund): resolve 500 Vite error on wizard page by using extends instead of component layout

--- packages/Remix/RefundRequest/src/Resources/views/customer/wizard-page.blade.php
+++ packages/Remix/RefundRequest/src/Resources/views/customer/wizard-page.blade.php
@@ -1,7 +1,7 @@
-<x-shop::layouts.account>
-    <x-slot:title>
-        Ajukan Refund
-    </x-slot>
+@extends('shop::layouts.account')
 
+@section('page_title', 'Ajukan Refund')
+
+@section('content')
     <livewire:remix-refund-request-wizard :order-id="$orderId" />
-</x-shop::layouts.account>
+@endsection
